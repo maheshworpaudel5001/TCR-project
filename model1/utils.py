f86976509af3138c1c69ff243f57e83035b8a1dc
maheshworpaudel5001 <maheshworpaudel5001@gmail.com>
2024-08-29 11:25:39 -0400

    simply rerranging

---
+++ model1/utils.py
@@ -0,0 +1,27 @@
+import numpy as np
+from scipy.integrate import quad
+
+def integrand_lymph(tau, x1, x2, kr, M):
+    p = np.exp(-x2 * kr * (x1 - tau))
+    integrand_value = np.exp(-tau) * p * ((1 - p) ** (M - 1))
+    return integrand_value
+
+def integrand_tumor(tau, x1_dagger, x2_dagger, kr, M):
+    p = np.exp(-x2_dagger * kr * (x1_dagger - tau))
+    integrand_value = np.exp(tau) * p * ((1 - p) ** (M - 1))
+    return integrand_value
+
+def probability_lymph(x1, x2, kr, M):
+    factor_up_front = 1.0 / (1 - np.exp(-x1))
+    integral = quad(integrand_lymph, 0, x1, args=(x1, x2, kr, M))[0]
+    return factor_up_front * integral
+
+def probability_tumor(x1_dagger, x2_dagger, kr, M):
+    factor_up_front = 1.0 / (np.exp(x1_dagger) - 1)
+    integral = quad(integrand_tumor, 0, x1_dagger, args=(x1_dagger, x2_dagger, kr, M))[0]
+    return factor_up_front * integral
+
+def probability(x1, x2, x1_dagger, x2_dagger, omega, kr, M):
+    tumor_probs = omega * probability_tumor(x1_dagger, x2_dagger, kr, M)
+    lymph_probs = (1 - omega) * probability_lymph(x1, x2, kr, M)
+    return tumor_probs + lymph_probs
---
+++ model2/utils.py
@@ -0,0 +1,45 @@
+from scipy.special import comb as comb
+import numpy as np
+from scipy.special import loggamma
+
+def probability(t, n0, n, Lambda, kr):
+    """Probability of a TCR with propensity kr to interact with neo-antigens to have frequency n at time t from n0 at time t=0
+
+    Args:
+        t (float): time at which probability is calculated
+        n0 (int/float): initial frequency of the TCR at t=0
+        n (int/float): final frequency of the TCR at t=t
+        Lambda (float): scaling constant determined as parameter from optimization
+        kr (float): provided from data
+
+    Returns:
+        float: probability
+    """
+    f = comb(n-1, n0-1)
+    exp1 = np.exp(-n0 * Lambda * kr * t)
+    exp2 = (1 - np.exp(-Lambda * kr * t))
+    prob = f * exp1 * (exp2**(n-n0))
+    return prob
+
+
+def log_probability(t, n0, n, Lambda, kr):
+    """
+    log_probability: log of the probability calculated from above function
+    
+    t (float): time at which probability is calculated
+        n0 (int/float): initial frequency of the TCR at t=0
+        n (int/float): final frequency of the TCR at t=t
+        Lambda (float): scaling constant determined as parameter from optimization
+        kr (float): provided from data
+
+    Returns:
+        float: log of probability
+    """
+    log_f1 = loggamma(n)
+    log_f2 = loggamma(n-n0+1)
+    log_f3 = loggamma(n0)
+    log_f = log_f1 - log_f2 - log_f3 #Log of combinatorics factor with Stirling's approximation applied
+    log_exp1 = -n0 * Lambda * kr * t
+    log_exp2 = (n-n0)*np.log(1 - np.exp(-Lambda * kr * t))
+    log_prob = log_f + log_exp1 + log_exp2
+    return log_prob
