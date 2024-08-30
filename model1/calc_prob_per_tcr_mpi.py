6e7fbadd9543d9850aebed5bcc2d677c9c4cb69e
maheshworpaudel5001 <maheshworpaudel5001@gmail.com>
2024-08-30 11:06:23 -0400

    added this file for version control

---
+++ model1/calc_prob_per_tcr_mpi.py
@@ -0,0 +1,165 @@
+import os
+import argparse
+import pandas as pd
+import numpy as np
+from mpi4py import MPI
+from probability import probability
+import h5py
+
+
+def calc_probs(x1, x2, x1_dagger, x2_dagger, omega, kr, M):
+    probabilities = [
+        probability(x1, x2, x1_dagger, x2_dagger, omega, kr, M_value)
+        for M_value in range(1, int(M) + 1)
+    ]
+    return probabilities
+
+
+def calc_probs_parallel(args):
+    x1, x2, x1_dagger, x2_dagger, omega, kr, M = args
+    result = calc_probs(x1, x2, x1_dagger, x2_dagger, omega, kr, M)
+    return result
+
+
+def process_subchunks(subchunk, x1, x2, x1_dagger, x2_dagger, omega, M):
+    results = []
+    for kr_value in subchunk:
+        local_results = calc_probs_parallel(
+            (x1, x2, x1_dagger, x2_dagger, omega, kr_value, M)
+        )
+        results.append(local_results)
+    return results
+
+
+if __name__ == "__main__":
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+
+    if not MPI.Is_initialized():
+        if rank == 0:
+            print(
+                "MPI is not initialized. Make sure you have OpenMPI installed and properly configured."
+            )
+        MPI.Finalize()
+
+    if rank == 0:
+        print("MPI is initialized and running.")
+
+    parser = argparse.ArgumentParser(description="Calculate probabilities.")
+    parser.add_argument("--class_id", type=str, help="class id")
+    parser.add_argument("--patient_id", type=str, required=True, help="patient id")
+    parser.add_argument(
+        "--o",
+        type=str,
+        default=os.path.join(os.getcwd(), "output_probabilities"),
+        help="Output filename: Give full path. Otherwise defaults to output_probabilities.h5 at current working directory.",
+    )
+
+    args = parser.parse_args()
+    class_id = args.class_id
+    patient_id = args.patient_id
+    output_filename = args.o
+
+    ##################Prepare data###################
+    full_data = pd.read_csv(
+        f"/home/gddaslab/mxp140/TCR_project/data/full_{class_id}_data_v2.csv",
+        sep=",",
+        comment="#",
+    )
+    if "all" not in patient_id:
+        patient_data = full_data[
+            full_data["CDR3"].str.contains(patient_id)
+        ].reset_index()
+    else:
+        patient_data = full_data
+
+    params_df = pd.read_csv(
+        "/home/gddaslab/mxp140/TCR_project/outputs/params_piecewise_omega_constrained_v2.csv",
+        sep=",",
+    )
+    patient_params_df = params_df[params_df["Patient_ID"] == patient_id]
+    x1_value = patient_params_df["x1"].values[0]
+    x2_value = patient_params_df["x2"].values[0]
+    x1_dagger_value = patient_params_df["x1_dagger"].values[0]
+    x2_dagger_value = patient_params_df["x2_dagger"].values[0]
+    omega_value = patient_params_df["w"].values[0]
+
+    M_max_values = {
+        "brmet008": 10000,
+        "brmet009": 10000,
+        "brmet010": 10000,
+        "brmet018": 10000,
+        "brmet019": 10000,
+        "brmet025": 10000,
+        "brmet027": 10000,
+        "brmet028": 10000,
+        "gbm032": 10000,
+        "gbm052": 10000,
+        "gbm055": 10000,
+        "gbm056": 10000,
+        "gbm059": 10000,
+        "gbm062": 10000,
+        "gbm063": 10000,
+        "gbm064": 10000,
+        "gbm070": 10000,
+        "gbm074": 10000,
+        "gbm079": 10000,
+    }
+    M_max_value = M_max_values.get(patient_id, 1000)
+
+    scaled_kr_values = patient_data["scaled_kr"].values
+    if rank == 0:
+        chunked_scaled_kr_values = [
+            scaled_kr_values
+        ]  # Ensure it's a list for consistency
+        if len(scaled_kr_values) > 10000:
+            # Further divide each chunk into subchunks, divided into size chunks here
+            chunked_scaled_kr_values = np.array_split(scaled_kr_values, size)
+        else:
+            # Ensure the list has 'size' elements, even if some are empty
+            chunked_scaled_kr_values = np.array_split(scaled_kr_values, size)
+    else:
+        chunked_scaled_kr_values = None
+
+    chunked_scaled_kr_values_scattered = comm.scatter(chunked_scaled_kr_values, root=0)
+
+    results = []
+    for kr_chunk in chunked_scaled_kr_values_scattered:
+        sub_results = process_subchunks(
+            kr_chunk,
+            x1_value,
+            x2_value,
+            x1_dagger_value,
+            x2_dagger_value,
+            omega_value,
+            M_max_value,
+        )
+        results.extend(sub_results)
+
+    # Gather results from all processes
+    results = comm.gather(results, root=0)
+
+    # Root process saves results
+    if rank == 0:
+        # Flatten the gathered results list
+        results = [item for sublist in results for item in sublist]
+
+        # Ensure the filename has the .h5 extension
+        if not output_filename.endswith(".h5"):
+            output_filename += ".h5"
+
+        # Save as HDF5 file
+        results_array = np.array(results)
+        with h5py.File(output_filename, "w") as f:
+            f.create_dataset("results", data=results_array)
+
+        print(
+            "Results saved to HDF5 file:",
+            output_filename,
+            " with a key called 'results'.",
+        )
+        print("Full shape:", results_array.shape)
+
+    # Finalize MPI
+    MPI.Finalize()
